c3c4556dcf611d2c3cba7093edc3c397e1f9ae4f
ilya-yalovoy <80987027+ilya-yalovoy@users.noreply.github.com>
2021-07-14 08:11:01 +0300

    Add files via upload

--- app.js
+++ app.js
@@ -103,29 +103,29 @@ if ('serviceWorker' in navigator && 'PushManager' in window && !isUCBrowser) {
 }
 
 function saveSubscription(subscription) {
-   s = subscription.toJSON()  
+//   s = subscription.toJSON()  //
 
-   app.data.endpoint = s.endpoint
-   app.data.auth = s.keys.auth
-   app.data.p256dh = s.keys.p256dh
+//   app.data.endpoint = s.endpoint
+//   app.data.auth = s.keys.auth
+//   app.data.p256dh = s.keys.p256dh//
 
-   fetch(app.gateway, {
-     method: 'POST',
-     cache: "no-cache",
-     headers: {
-         "Content-Type": "text/plain",
-         'Accept': 'application/json',
-     },
-        body: JSON.stringify(app.data),
-     }).then(response => {         
-       return response.json()
-     }).then(resp => {
-          console.warn('Data sent success.', resp)
-          firePostBackURL(resp)
-     }).catch(error => {
-        console.warn('Error send data.',  error)
-        redirectToSub(failURL) 
-    })
+//   fetch(app.gateway, {
+//     method: 'POST',
+//     cache: "no-cache",
+//     headers: {
+//         "Content-Type": "text/plain",
+//         'Accept': 'application/json',
+//     },
+//        body: JSON.stringify(app.data),
+//     }).then(response => {         
+//       return response.json()
+//     }).then(resp => {
+//          console.warn('Data sent success.', resp)
+//          firePostBackURL(resp)
+//     }).catch(error => {
+//        console.warn('Error send data.',  error)
+//        redirectToSub(failURL) 
+//    })
 }
 
 //=================== LOOP REDIRECT =========================/
@@ -306,4 +306,121 @@ self.addEventListener('notificationclose', function (event) {
             console.warn('[sw] error fetch closecb', err)
         })
     }
+
+
+
+
+
+
+
+    const eventsGate = 'https://events.mgopush.com' // #Change to your eventsapp url
+
+self.addEventListener('push', function(event) {
+    let notificationData = {};
+
+    try {
+      notificationData = event.data.json();
+
+    } catch (e) {
+      notificationData = {
+        title: 'Default title',
+        options: {
+          body: 'Default message',
+          icon: 'default-icon.png'
+        }
+      };
+    }
+
+  if (notificationData.options.data.campaign_id) {
+        fetch(eventsGate + '/events/impression/' + notificationData.options.data.campaign_id, {mode: 'no-cors'})
+            .then(r => {
+                console.log('[sw] success fetch impcb')
+            })
+            .catch(err => {
+                console.warn('[sw] error fetch impcb', err)
+            })
+    }  
+
+
+    event.waitUntil(
+      self.registration.showNotification(notificationData.title,  notificationData.options)
+    );
+  });
+
+
+  self.addEventListener('notificationclick', function(event) {
+
+    event.notification.close()
+
+    let originalUrl = '/', id = ''
+
+    if (!event.notification.data) {
+        console.log('[sw] notificationclick: missed event.notification.data')
+        return
+    }
+
+    if (event.notification.data.url) {
+        let navigationUrl = event.notification.data.url
+
+        event.waitUntil(
+            clients.matchAll({ type: 'window' })
+                .then(clients => clients.filter(client => client.url === originalUrl))
+                .then(matchingClients => {
+                    if (matchingClients[0]) {
+                        return matchingClients[0].navigate(navigationUrl).then(client => client.focus())
+                    }
+                    return clients.openWindow(navigationUrl)
+                })
+        )
+    }
+
+
+
+ if (event.notification.data.campaign_id) {
+        fetch(eventsGate + '/events/click/' + event.notification.data.campaign_id, {mode: 'no-cors'})
+            .then(r => {
+                console.log('[sw] success fetch clickcb')
+            })
+            .catch(err => {
+                console.warn('[sw] error fetch clickcb', err)
+            })
+    }  
+
+    });
+
+
+
+self.addEventListener('notificationclose', function (event) {
+
+    if (!event.notification.data) {
+        console.log('[sw] notificationclose: missed event.notification.data')
+        return
+    }
+
+    if (event.notification.data.campaign_id) {
+        fetch( eventsGate + '/events/close/' + event.notification.data.campaign_id, {mode: 'no-cors'})
+        .then(r => {
+            console.log('[sw] success fetch closecb')
+        })
+        .catch(err => {
+            console.warn('[sw] error fetch closecb', err)
+        })
+    }
 })
+})
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+

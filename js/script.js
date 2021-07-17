e209e73b2d06581603532b4b5587aabbce89c434
ilya-yalovoy <80987027+ilya-yalovoy@users.noreply.github.com>
2021-07-17 20:06:41 +0300

    Add files via upload

--- js/script.js
+++ js/script.js
@@ -1,8 +1,18 @@
 $(document).ready(function () {
 	$('.burger__menu').click(function(event) {
 		$('.burger__menu,.menu').toggleClass('active');
+		$('body').toggleClass('no-active');
 	});
 });
+
+window.onload = function() {
+	$('body').toggleClass('no-active');
+	$('.preloader').css('opacity', '0');
+	setTimeout(() => {
+		$('.preloader').css('display', 'none');
+	}, 1000);
+}
+
 
 const swiper = new Swiper('.swiper-container', {
 	// Optional parameters
@@ -52,15 +62,27 @@ const swiper = new Swiper('.swiper-container', {
 
 document.querySelector('#popap1-btn').addEventListener('click', function() {
 	$('.modal').addClass('active');
+	$('body').toggleClass('no-active');
 	$('.modal__form-close').click(function(event) {
 		$('.modal').removeClass('active');
+		$('body').toggleClass('no-active');
+	});
+})
+document.querySelector('#popap3').addEventListener('click', function() {
+	$('.modal').addClass('active');
+	$('body').toggleClass('no-active');
+	$('.modal__form-close').click(function(event) {
+		$('.modal').removeClass('active');
+		$('body').toggleClass('no-active');
 	});
 })
 
 document.querySelector('#popap4-btn').addEventListener('click', function() {
 	$('.modal__question').css('display', 'block');
+	$('body').toggleClass('no-active');
 	$('.modal__question-close').click(function(event) {
 		$('.modal__question').css('display', 'none');
+		$('body').toggleClass('no-active');
 	});
 })
 
@@ -68,4 +90,98 @@ document.querySelector('#popap4-btn').addEventListener('click', function() {
 $('.menu__burger').click(function(event) {
 	$('.menu__burger').toggleClass('active');
 	$('.menu').toggleClass('active');
+	$('body').toggleClass('no-active');
 });
+
+$('.menu_item').click(function(event) {
+	$('.menu__burger').toggleClass('active');
+	$('.menu').toggleClass('active');
+	$('body').toggleClass('no-active');
+});
+
+
+$('.faq-btn1').click(function(event) {
+	$('.faq1').toggleClass('off');
+	$('.faq-btn1').toggleClass('off');
+});
+
+$('.faq-btn2').click(function(event) {
+	$('.faq2').toggleClass('off');
+	$('.faq-btn2').toggleClass('off');
+});
+
+$('.faq-btn3').click(function(event) {
+	$('.faq3').toggleClass('off');
+	$('.faq-btn3').toggleClass('off');
+});
+
+$('.faq-btn4').click(function(event) {
+	$('.faq4').toggleClass('off');
+	$('.faq-btn4').toggleClass('off');
+});
+
+$('#popap-th-close').click(function(event) {
+	$('.popup-th').css('display', 'none');
+});
+
+$('.modal__form-btn').click(function() {
+	var a = document.querySelector('.modal__form-input-name'),
+		b = document.querySelector('.modal__form-input-tel');
+	if (a.value == '' || b.value == '') {
+		
+	} else {
+		$('.modal').removeClass('active');
+		$('body').toggleClass('no-active');
+		thank()
+	}
+})
+
+
+function thank() {
+	$('.popup-th').css('display', 'block');
+	setTimeout(() => {
+		$('.popup-th').css('display', 'none');
+	}, 3000);
+}
+
+$('.modal__question-btn').click(function() {
+	var a = document.querySelector('.modal__question-input-tel'),
+		c = document.querySelector('.modal__question-input-name'),
+		b = document.querySelector('.modal__question-text');
+	if (a.value == '' || b.value == '' || c.value == '') {
+		
+	} else {
+		$('.modal__question').css('display', 'none');
+		$('body').toggleClass('no-active');
+		thank()
+	}
+})
+
+
+$('#popap2-btn').click(function() {
+	$('.calc').css('display', 'block');
+	$('body').toggleClass('no-active');
+	$('#calc-close').click(function() {
+		$('.calc').css('display', 'none');
+		$('body').toggleClass('no-active');
+	})
+})
+
+
+const swiper3 = new Swiper('.calc-swiper', {
+	// Optional parameters
+  
+	// If we need pagination
+	pagination: {
+	  el: '.calc-swiper-pagination',
+	  type: 'fraction',
+	},
+
+	centeredSlides: true,
+	navigation: {
+	  nextEl: '.calc-swiper-button-next',
+	  prevEl: '.calc-swiper-button-prev',
+	},  
+	// And if we need scrollbar
+  });
+
